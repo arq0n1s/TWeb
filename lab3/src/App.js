d6cca55f778a3859dc3dd79f2108db53c7c46151
PavelliOn <p.lukiyan@yandex.ru>
2024-03-18 01:55:17 +0200

    added footer, navbars and applyButton in lab3 project

--- lab3/src/App.js
+++ lab3/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbars from './components/navbars';
+import Footer from './components/footer';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import ApplyButton from './components/apply_button';
+//import '../react/jsx-pascal-case';
+
+
+//rafce - components hotkey
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbars/>
+      <Footer message={'hello'}/>
+      <ApplyButton/>
     </div>
   );
 }
